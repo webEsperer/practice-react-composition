5af5864921c995d8bde3b7a0b14eff1fa402c3dd
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2022-11-17 13:42:00 +0100

    update to React v.18

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Category from './Category';
 import Cart from './Cart';
@@ -21,7 +21,5 @@ class App extends React.Component {
     }
 }
 
-ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
-);
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
