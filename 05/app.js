8cf121094a11aafa5038ee44a58c166f143e663c
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2022-11-17 13:42:06 +0100

    update to React v.18

--- 05/app.js
+++ 05/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Textarea from './Textarea';
 
@@ -16,4 +16,5 @@ class App extends React.Component {
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
